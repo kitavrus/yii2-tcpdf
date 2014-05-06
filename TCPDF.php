c1981340b5a6d95e104fac652785f37560480b75
cinghie <giando.olini@gogodigital.it>
2014-05-06 13:02:13 +0200

    Minor Update

--- TCPDF.php
+++ TCPDF.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ * @copyright Copyright &copy; Giandomenico Olin, gogodigital.it, 2014
+ * @package yii2-tcpdf
+ * @version 1.0.0
+ * @tcpdf library 6.0.075
+ */
+
 namespace cinghie\tcpdf;
 
 use Yii;
 use yii\base\Component;
 
+/*                                     *
+ *  Component to load TCPDF Libraries  *
+ *                                     */
+ 
 class TCPDF extends Component 
 {
 
